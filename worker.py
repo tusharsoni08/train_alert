95128c6ad9a6a8d513891c27ae7b65488dab7a55
tusharsoni08 <soni.tushar93@gmail.com>
2018-08-10 21:02:01 +0530

    changed redis host

--- worker.py
+++ worker.py
@@ -5,9 +5,9 @@ from rq import Worker, Queue, Connection
 
 listen = ['default']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+#redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
-conn = redis.from_url(redis_url)
+conn = redis.Redis()
 
 if __name__ == '__main__':
     with Connection(conn):
