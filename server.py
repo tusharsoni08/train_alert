0a47896474a677f61efc6c4eef106c495420bf32
tusharsoni08 <soni.tushar93@gmail.com>
2018-08-18 02:56:34 +0530

    Changed confirmation message

--- server.py
+++ server.py
@@ -129,7 +129,7 @@ def processDetails(pnr, userId):
 
 	if response_status == True:
 		print("Okay, I've set the reminder. Enjoy your journey!")
-		return endConversation("Okay, I've set the reminder. Enjoy your journey!")
+		return endConversation("Okay, I've set the reminder and will send you a push notification before your train reaches the destination station. Enjoy your journey!")
 	elif response_status == None:
 		print("Sorry, your train has departed from your destination station.")
 		return endConversation("Sorry, your train has departed from your destination station.")
